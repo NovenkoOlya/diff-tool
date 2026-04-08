73c5c92a484d3e25b7a6ce32889c0b48fe58bdf5
Olha <olykagreen220@gmail.com>
2026-04-08 18:54:16 +0300

    Add second Pytest

--- tests/test_comparator.py
+++ tests/test_comparator.py
@@ -10,3 +10,20 @@ def temp_files(tmp_path):
     f1.write_text("apple\nbanana\ncherry", encoding="utf-8")
     f2.write_text("banana\ndate\ncherry", encoding="utf-8")
     return f1, f2
+
+@pytest.mark.parametrize("set1, set2, expected", [
+    ({"line1", "line2"}, {"line2", "line3"}, {"line2"}),
+    ({"a"}, {"b"}, set()),
+    (set(), {"a"}, set())
+])
+
+def test_get_intersections(set1, set2, expected):
+    assert get_intersections(set1, set2) == expected
+
+@pytest.mark.parametrize("set1, set2, expected", [
+    ({"line1", "line2"}, {"line2", "line3"}, {"line1", "line3"}),
+    ({"a"}, {"a"}, set()),
+    ({"a"}, set(), {"a"})
+])
+def test_get_differences(set1, set2, expected):
+    assert get_differences(set1, set2) == expected
