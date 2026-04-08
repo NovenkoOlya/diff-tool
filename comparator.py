5a813fff8e784012109e69735dd2744a3cee92f2
Olha <olykagreen220@gmail.com>
2026-04-08 18:32:27 +0300

    Add intersection logic

--- comparator.py
+++ comparator.py
@@ -6,3 +6,7 @@ def read_file_lines(file_path):
         return set()
     with open(file_path, 'r', encoding='utf-8') as f:
         return {line.strip() for line in f if line.strip()}
+    
+def get_intersections(set1, set2):
+    """Повертає спільні рядки."""
+    return set1.intersection(set2)
